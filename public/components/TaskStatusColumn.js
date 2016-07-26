4b41433ca996e01968b2f155a1cfc26816d4b802
Heather Park <hthr.prk@gmail.com>
2016-07-25 17:33:50 -0700

    Clean up code and add comments

--- public/components/TaskStatusColumn.js
+++ public/components/TaskStatusColumn.js
@@ -2,12 +2,14 @@ import React from 'react';
 import TaskCard from './TaskCard';
 import AddTask from './AddTask';
 
+// es6 shorthand for giving corresponding variable names to props
 const TaskStatusColumn = ({tasks, columnName}) => {
   return (
     <div className="task-column">
       <div className="column-title">
         <p>{columnName}</p>
       </div>
+      // map through "tasks" props to add each one to the column
       {tasks.map((task, index) =>
         <TaskCard task={task} key={index} />
       )}
