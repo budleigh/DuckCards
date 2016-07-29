cb30e7370156d29cca61ac2756839e790796bee5
graftss <coughcoughcough@gmail.com>
2016-07-29 14:30:53 -0400

    (feat) add initial react router

--- public/index.js
+++ public/index.js
@@ -1,15 +1,18 @@
 // import 'babel-polyfill'
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import App from './containers/app'
-import configureStore from './store/configureStore'
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './containers/app';
+import configureStore from './store/configureStore';
+import { hashHistory, Router, Route } from 'react-router';
 
-const store = configureStore()
+const store = configureStore();
 
 render(
   <Provider store={ store }>
-    <App />
+    <Router history={ hashHistory }>
+      <Route path="/" component={ App } />
+    </Router>
   </Provider>,
   document.getElementById('app')
-)
+);
