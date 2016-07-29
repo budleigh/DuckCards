c7e101f1afcfd9013902c9d6699843563da46608
Patrick Truong <pahtruon@gmail.com>
2016-07-28 23:10:40 -0700

    (refactor) add users array to task(gonna be named project) schema

--- Server/db/db.js
+++ Server/db/db.js
@@ -6,31 +6,26 @@ var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 
 var taskSchema = new Schema({
-  id: ObjectId, // WTH is this
-  title: String,
-  status: String,
-  dueDate: String,
-  category: String,
-  points: Number,
-  notes: String,
-  owner: String,
-  creator: String,
-  comments: [{
-      user: String,
-      comment: String
+  id: ObjectId,
+  name: String,
+  tasks: [{
+    title: String,
+    status: String,
+    dueDate: String,
+    category: String,
+    points: Number,
+    notes: String,
+    owner: String,
+    creator: String,
+    comments: [{
+        user: String,
+        comment: String
+      }]
     }],
-  projects: Number
+  users: [String]
 });
-
-
-var projectSchema = new Schema({
-  id: Number,
-  name: String
-});
-
 
 var Task = mongoose.model('Task', taskSchema);
-var Project = mongoose.model('Project', projectSchema);
 
 mongoose.connect(url);
 
