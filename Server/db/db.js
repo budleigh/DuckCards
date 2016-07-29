962615bfe35a5f95b99c4c426173a740ad21f92e
Patrick Truong <pahtruon@gmail.com>
2016-07-29 12:41:10 -0700

    (refactor) move project schema into seperate file

--- Server/db/db.js
+++ Server/db/db.js
@@ -1,31 +1,5 @@
 var mongoose = require("mongoose");
-var bcrypt = require("bcrypt-nodejs");
 var url = process.env.MONGODB_URI || 'mongodb://localhost/appdb';
-
-var Schema = mongoose.Schema;
-var ObjectId = Schema.ObjectId;
-
-var taskSchema = new Schema({
-  id: ObjectId,
-  name: String,
-  tasks: [{
-    title: String,
-    status: String,
-    dueDate: String,
-    category: String,
-    points: Number,
-    notes: String,
-    owner: String,
-    creator: String,
-    comments: [{
-        user: String,
-        comment: String
-      }]
-    }],
-  users: [String]
-});
-
-var Task = mongoose.model('Task', taskSchema);
 
 mongoose.connect(url);
 
