be7136178368a41f5ccdb6e84d4804139167ca73
budleigh <arguewithplato@gmail.com>
2016-07-29 20:05:54 -0700

    (feat) add dashboard html

--- public/components/Dashboard/Dashboard.js
+++ public/components/Dashboard/Dashboard.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { fetchProjects } from '../../actions/projects';
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
   }
 
+  componentDidMount() {
+    this.props.dispatch(fetchProjects());
+  }
+
   render () {
     return (
-      <div>hello</div>
+      <div>
+        {this.props.projects.map((project) => {
+          <div>{ project }</div>
+        })}
+      </div>
     );
   }
 }
