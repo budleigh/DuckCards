e75c243a8b5bf4e884f5ce18d693a134bf867f83
budleigh <arguewithplato@gmail.com>
2016-07-29 19:16:16 -0700

    (feat) add dashboard reducer

--- public/reducers/index.js
+++ public/reducers/index.js
@@ -51,8 +51,28 @@ function user (state = {}, action) {
   }
 }
 
+function dashboard (state = {}, action) {
+  switch (action.type) {
+    case REQUEST_PROJECTS:
+      return Object.assign({}, state, {
+        isFetching: true
+      });
+
+    case RECEIVE_PROJECTS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        data: action.data,
+        lastUpdated: Date.now()
+      });
+
+    default:
+      return state;
+  }
+}
+
 export default combineReducers({
   authForm,
   tasks,
-  user
+  user,
+  dashboard
 });
