63d373b80f11ddcb3af31e153b771412cef384fa
budleigh <arguewithplato@gmail.com>
2016-07-30 11:40:48 -0700

    (feat) load/fetch project actions

--- public/actions/projects.js
+++ public/actions/projects.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const REQUEST_PROJECTS = 'REQUEST_PROJECTS';
 export const RECEIVE_PROJECTS = 'RECEIVE_PROJECTS';
+export const LOAD_PROJECT = 'LOAD_PROJECT';
 
 export function requestProjects() {
   return {
@@ -17,12 +18,27 @@ export function receiveProjects(response) {
   };
 }
 
+export function loadProject(response) {
+  return {
+    type: LOAD_PROJECT,
+    data: response.data
+  };
+}
+
 export function fetchProjects() {
   return dispatch => {
     return axios.get('/users/projects')
       .then( response => dispatch(receiveProjects(response)) )
       .catch( error => console.log(error) );
   };
+}
+
+export function fetchProject(projectId) {
+  return dispatch => {
+    return axios.get('/projects/' + projectId)
+      .then( response => dispatch(loadProject(response)) )
+      .catch( error => console.log(error) );
+  }
 }
 
 export function createProject(name) {
--- public/reducers/index.js
+++ public/reducers/index.js
@@ -3,10 +3,12 @@ import authForm from './authForm';
 import dashboard from './dashboard';
 import tasks from './tasks';
 import user from './user';
+import project from './project';
 
 export default combineReducers({
   authForm,
   dashboard,
   tasks,
+  project,
   user
 });
