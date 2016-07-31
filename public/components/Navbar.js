6e0a17994aabf2bd304ca80f7343e95e632c4554
budleigh <arguewithplato@gmail.com>
2016-07-31 12:35:39 -0700

    (feat) create home link on appbar

--- public/components/Navbar.js
+++ public/components/Navbar.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { AppBar, RaisedButton, Avatar, FlatButton } from 'material-ui';
+import { connect, hashHistory } from 'react-redux';
+import { AppBar, RaisedButton, Avatar, FlatButton, IconButton } from 'material-ui';
+import ActionHome from 'material-ui/svg-icons/action/home';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import TaskModal from './TaskModal';
 import { setVisibility, setMode, clearAllFields } from '../actions/taskModal';
+import { Link } from 'react-router';
 
 //this allows an onTouchTap event on some of material-UI's components
 
 injectTapEventPlugin();
-
-const style = {
-  marginRight: 20
-}
 
 class Nav extends React.Component {
   constructor(props) {
@@ -25,11 +28,11 @@ class Nav extends React.Component {
       <AppBar
         title={"Falafel.io"}
         className="navBar"
-        style={{position: 'fixed'}}
+        iconElementLeft={<IconButton containerElement={<Link to="/" />}><ActionHome /></IconButton>}
         iconElementRight={
-          <div style={style} >
+          <div>
             <TaskModal />
-            <RaisedButton label="Add New Task" onTouchTap={openCreateTaskModal} />
+            <IconButton onTouchTap={openCreateTaskModal}><ContentAdd /></IconButton>
           </div>
         }
       />
