c3fae312ee9a3f80de3eb0c2ed4987665ce4d0a4
Patrick Truong <pahtruon@gmail.com>
2016-07-30 18:57:47 -0700

    (style) basic style for project dashboard

--- public/components/Dashboard/ProjectCreator.js
+++ public/components/Dashboard/ProjectCreator.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { TextField, RaisedButton } from 'material-ui';
 
 const ProjectCreator = ({ onCreate }) => {
   let input;
 
   return (
-    <div>
-      Create project
-        <form>
-          <input ref={node => { input = node; }} type="text" name="name" placeholder="name" />
-        </form>
-        <button onClick={() => {
-          onCreate(input.value);
-          input.value = '';
-        }}>
-          create
-        </button>
-    </div>
+    <MuiThemeProvider>
+      <div>
+        <h2>Create project</h2>
+          <form>
+            <input ref={node => { input = node; }} type="text" name="name" placeholder="name" />
+          </form>
+          <RaisedButton onClick={() => {
+            onCreate(input.value);
+            input.value = '';
+          }}
+            type="submit"
+            primary={true}
+            label="Create"
+          />
+      </div>
+    </MuiThemeProvider>
   );
 };
 
