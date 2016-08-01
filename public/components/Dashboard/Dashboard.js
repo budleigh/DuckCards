046dea30d75a7435eb2251e1a6310a2b0e662c08
budleigh <arguewithplato@gmail.com>
2016-07-31 20:44:26 -0700

    (feat) add logout button to dashboard

--- public/components/Dashboard/Dashboard.js
+++ public/components/Dashboard/Dashboard.js
@@ -8,6 +8,7 @@ import {List, ListItem} from 'material-ui/List';
 import Divider from 'material-ui/Divider';
 import { FlatButton } from 'material-ui';
 import { signout } from '../../auth';
+import { withRouter } from 'react-router';
 
 class Dashboard extends Component {
   constructor(props) {
@@ -22,11 +23,16 @@ class Dashboard extends Component {
     this.props.dispatch(createProject(name));
   }
 
+  signOut() {
+    signout();
+    this.props.router.replace('/signin');
+  }
+
   render () {
     return (
       <MuiThemeProvider>
         <div className="projects">
-          <FlatButton className="logout-button" label="Logout" secondary={true} />
+          <FlatButton className="logout-button" label="Logout" secondary={true} onClick={this.signOut.bind(this)} />
           <List className="project-list">
             {this.props.projects.map((project) => {
               return (<div><ListItem className="project"><Link to={ '/project/' + project._id }>{ project.name }</Link></ListItem><Divider /></div>);
@@ -45,4 +51,4 @@ Dashboard.propTypes = {
 
 const mapStateToProps = state => state.dashboard;
 
-export default connect(mapStateToProps)(Dashboard);
+export default connect(mapStateToProps)(withRouter(Dashboard));
